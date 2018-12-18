31c7f80b785b9a32de62b0d29f38561de28fc2d5
KhoaPham <dk_fallenangel_2111@yahoo.com>
2018-12-18 17:45:36 +0700

    update SEO

--- VASTbit Rebuild/assets/js/mymap.js
+++
@@ -1,2 +0,0 @@
-function myMap()
-{myCenter=new google.maps.LatLng(10.7990563,106.6787713);var mapOptions={center:myCenter,zoom:14,scrollwheel:!0,draggable:!1,mapTypeId:google.maps.MapTypeId.ROADMAP};var map=new google.maps.Map(document.getElementById("map"),mapOptions);var marker=new google.maps.Marker({position:myCenter,});marker.setMap(map)}
